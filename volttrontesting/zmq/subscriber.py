fa553a35b0cf1605b6f4af36422e4fa0e4e336f1
Craig Allwardt <craig.allwardt@pnnl.gov>
2016-06-03 12:01:11 -0700

    modified subscriber to get address,port from command line

--- volttrontesting/zmq/subscriber.py
+++ volttrontesting/zmq/subscriber.py
@@ -1,12 +1,21 @@
 import sys
 import zmq.green as zmq
+address = None
+while address not in (1, 2):
+    address = input('Enter subscription address (1=tcp://127.0.0.1, 2=tcp://127.0.0.2):')
 
+if address == 1:
+    address = "tcp://127.0.0.1"
+else:
+    address = "tcp://127.0.0.2"
+
+port = raw_input('Enter subscription port: ')
 # Socket to talk to server
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 print "Collecting updates from server..."
 #addr = "ipc://@/home/vdev/.volttron/run/publish"
-addr = "tcp://127.0.0.2:5000"
+addr = "%s:%s"%(address, port)
 print(addr)
 socket.connect(addr)
 topicfilter = ""
