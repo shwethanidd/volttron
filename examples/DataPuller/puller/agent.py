22608f78fda726d86dbb5ed7f02b8fd3ac13762e
shwethanidd <shwetha.niddodi@pnnl.gov>
2017-03-03 14:34:38 -0800

    Update agent.py

--- examples/DataPuller/puller/agent.py
+++ examples/DataPuller/puller/agent.py
@@ -233,33 +233,13 @@ def historian(config_path, **kwargs):
                                 headers=headers,
                                 message=payload['message']).get(30)
 
-
-        # def puller_setup(self):
-        #     try:
-        #         _log.debug("Setting up to pull from {}".format(source_vip))
-        #         event = gevent.event.Event()
-        #         keystore = KeyStore()
-        #         agent = Agent(address=source_vip)
-        #         agent.core.onstart.connect(lambda *a, **kw: event.set(), event)
-        #         gevent.spawn(agent.core.run)
-        #         event.wait(timeout=10)
-        #         self._target_platform = agent
-        #     except gevent.Timeout:
-        #         self.vip.health.set_status(
-        #                     STATUS_BAD, "Timeout in setup of agent")
-        #         status = Status.from_json(self.vip.health.get_status())
-        #         self.vip.health.send_alert(FORWARD_TIMEOUT_KEY,
-        #                                        status)
-
         def puller_setup(self):
             _log.debug("Setting up to forward to {}".format(source_vip))
-            keystore = KeyStore()
-            #_log.debug("Public key: {0}, Secret key: {1}".format(self.core.pubickey, self.core.secretkey))
             try:
                 agent = build_agent(address=source_vip,
                                     serverkey=source_serverkey,
-                                    publickey=keystore.public,
-                                    secretkey=keystore.secret,
+                                    publickey=self.core.publickey,
+                                    secretkey=self.core.secretkey,
                                     enable_store=False)
             except gevent.Timeout:
                 self.vip.health.set_status(
