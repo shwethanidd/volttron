b004a3a3214dfc08e04749466b78286f6365a3f2
shwethanidd <shwetha.niddodi@pnnl.gov>
2017-05-04 18:39:25 +0000

    Added upgrade all utility script

---
+++ upgrade-all.sh
@@ -0,0 +1,7 @@
+./my_config/upgrade-actuator
+./my_config/upgrade-alerter
+./my_config/upgrade-override
+#./config/upgrade-emailer
+#./my_config/upgrade-agentwatcher
+./my_config/upgrade-threshold
+./my_config/upgrade-scheduler
